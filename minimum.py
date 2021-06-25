ce16c3bb5505b580057f61eeb88b62eb0e3a39aa
pome-ta <pome-ta@users.noreply.github.com>
2021-06-26 00:45:54 +0900

    minimum.py

--- minimum.py
+++ minimum.py
@@ -1,30 +1,67 @@
-from objc_util import load_framework, ObjCClass, ObjCInstance, on_main_thread, CGRect
+from objc_util import load_framework, ObjCClass, ObjCInstance, on_main_thread
 import ui
 import pdbg
 
 load_framework('SceneKit')
 
+SCNView = ObjCClass('SCNView')
+SCNScene = ObjCClass('SCNScene')
+SCNNode = ObjCClass('SCNNode')
+SCNCamera = ObjCClass('SCNCamera')
+SCNLight = ObjCClass('SCNLight')
+SCNBox = ObjCClass('SCNBox')
+
+UIColor = ObjCClass('UIColor')
+
+
 class SceneView:
   @on_main_thread
   def __init__(self):
-    self.SCNScene = ObjCClass('SCNScene')
-    self.SCNView = ObjCClass('SCNView')
-    
+    self.view = self.create_view()
     self.view_did_load()
 
-  def view_did_load(self):
-    # --- View
-    frame = CGRect((0, 0), (100, 100))
-    flex_w, flex_h = (1 << 1), (1 << 4)
-    self.scn_view = self.SCNView.alloc()
-    self.scn_view.initWithFrame_options_(frame, None)
-    #self.scn_view.autorelease()
-    self.scn_view.autoresizingMask = (flex_w | flex_h)
-    self.scn_view.allowsCameraControl = True
-    self.scn_view.showsStatistics = True
+  def create_view(self):
+    scnView = SCNView.alloc()
+    scnView.initWithFrame_options_(((0, 0), (100, 100)), None)
+    #self.scnView.autorelease()
+    scnView.autoresizingMask = ((1 << 1) | (1 << 4))
+    scnView.backgroundColor = UIColor.blackColor()
+    scnView.allowsCameraControl = True
+    scnView.showsStatistics = True
+    '''
+    OptionNone = 0
+    ShowPhysicsShapes = (1 << 0)
+    ShowBoundingBoxes = (1 << 1)
+    ShowLightInfluences = (1 << 2)
+    ShowLightExtents = (1 << 3)
+    ShowPhysicsFields = (1 << 4)
+    ShowWireframe = (1 << 5)
+    RenderAsWireframe = (1 << 6)
+    ShowSkeletons = (1 << 7)
+    ShowCreases = (1 << 8)
+    ShowConstraints = (1 << 9)
+    ShowCameras = (1 << 10)
+    '''
+    scnView.debugOptions = (1 << 1) | (1 << 3) | (1 << 5) | (1 << 6) | (1 << 10)
+    return scnView
 
-    scene = self.SCNScene.new()
-    self.scn_view.scene = scene
+  def view_did_load(self):
+    scene = SCNScene.scene()
+
+    cameraNode = SCNNode.node()
+    cameraNode.camera = SCNCamera.camera()
+    cameraNode.position = (0.0, 0.0, 25.0)
+    scene.rootNode().addChildNode_(cameraNode)
+
+    box = SCNBox.box()
+    box.width = 1
+    box.height = 1
+    box.length = 1
+    boxNode = SCNNode.node()
+    boxNode.geometry = box
+    scene.rootNode().addChildNode_(boxNode)
+
+    self.view.scene = scene
 
 
 class View(ui.View):
@@ -33,9 +70,10 @@ class View(ui.View):
     self.bg_color = 'maroon'
     self.instance = ObjCInstance(self)
     scn = SceneView()
-    self.instance.addSubview_(scn.scn_view)
+    self.instance.addSubview_(scn.view)
 
 
 if __name__ == '__main__':
   view = View()
   view.present(style='fullscreen', orientations=['portrait'])
+
